58de767519638e223b9ea0cadba69127ee03f24f
Trevor Tabaka <trevortabaka@Trevors-MacBook-Pro.local>
2018-04-05 15:45:03 +0800

    fixed exception for the ConversionType. Added the getters and setters

--- src/main/java/mybatis/model/cryptocompare/histohour/ConversionType.java
+++ src/main/java/mybatis/model/cryptocompare/histohour/ConversionType.java
@@ -5,4 +5,19 @@ public class ConversionType {
     String type;
     String conversionSymbol;
 
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getConversionSymbol() {
+        return conversionSymbol;
+    }
+
+    public void setConversionSymbol(String conversionSymbol) {
+        this.conversionSymbol = conversionSymbol;
+    }
 }
